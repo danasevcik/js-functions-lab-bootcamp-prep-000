1c7f00d380aeb450f8ac34d3e8262e2a6a9fe83d
danasevcik@hotmail.com <danasevcik@hotmail.com>
2018-09-11 02:23:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!'
 }
-happyHolidaysTo('you')
 
 function happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!'
