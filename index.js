31f4809a461b27d285f37fbd6b33ea95cf9650d2
danasevcik@hotmail.com <danasevcik@hotmail.com>
2018-09-11 01:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return 'Happy holidays, name!'
 }
 happyHolidaysTo('you')
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
+  return 'Happy holiday, name!'
 }
 
 function holidayCountdown(holiday, days) {
-  return "It's ${days} days until ${holiday}!"
+  return "It's days days until holiday!"
 }
 
 happyHolidaysTo('you')
